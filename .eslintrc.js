6b4b8a4b1a54e1e5ebffd1c30e7411b30f524ef8
Yarik Genza <gsmeter@gmail.com>
2018-02-25 12:44:35 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'class-methods-use-this': 1,
+    'consistent-return': 1,
+    'function-paren-newline': 1,
+    "react/prop-types": 0,
+    "react/jsx-filename-extension": 0,
+    "react/prefer-stateless-function": 0,
+    "no-use-before-define": 1,
+    'no-console': 1,
+    'no-else-return': 1,
+    'no-param-reassign': 1,
+    'no-underscore-dangle': ['error', { 'allow': ['_id', '__STATE'] }],
+    'object-curly-newline': 0,
+    'semi': ['error', 'always'],
+    "import/no-unresolved": "off",
+    "import/extensions": "off"
+  },
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    mocha: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: [
+          'node_modules',
+          __dirname,
+        ]
+      }
+    }
+  }
+};
