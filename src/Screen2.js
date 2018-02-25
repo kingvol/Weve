fdb6fa408b05d06bb4370502c2b65d2c6f062432
Yarik Genza <gsmeter@gmail.com>
2018-02-25 15:20:56 +0200

    integrate nav to android

---
+++ src/Screen2.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export default class Screen2 extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          SCREEN 2 YAY!!!
+        </Text>
+      </View>
+    );
+  }
+}
