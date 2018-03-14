5eeba4ddaf8f0c14646a6b95713c1b29783c7858
Preeternal <preeternal@ya.ru>
2018-03-14 20:04:54 +0200

    add fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import { Text, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 import { Button } from './Components/Common';
+import { primaryFont } from './Theme';
 
 export default class App extends Component {
   componentDidMount() {
@@ -15,12 +13,12 @@ export default class App extends Component {
   render() {
     return (
       <View>
-        <Text>
-          Welcome to React Native!
-        </Text>
-        <Button><Text>hello from button</Text></Button>
+        <Text style={{ fontFamily: 'Cochin' }}>Welcome to React Native!</Text>
+        <Text style={{ fontFamily: primaryFont.fontFamily }}>Welcome to React Native!</Text>
+        <Button>
+          <Text>hello from button</Text>
+        </Button>
       </View>
     );
   }
 }
-
