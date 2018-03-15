facba2fe4d4c6aea3cbc8eb4bd66fc9d1f4b7d95
Yarik Genza <gsmeter@gmail.com>
2018-03-15 16:02:38 +0200

    setup appcenter sdk

--- android/app/src/main/java/com/wevedo_app/MainApplication.java
+++ android/app/src/main/java/com/wevedo_app/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.support.annotation.NonNull;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,7 +29,10 @@ public class MainApplication extends NavigationApplication  {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),            
+          new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),            
             new SplashScreenReactPackage()
       );
     }
