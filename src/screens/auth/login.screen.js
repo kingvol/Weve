01ef7baffdb6cd88598d17e996e4f2868c668a35
Yarik Genza <gsmeter@gmail.com>
2018-03-18 01:03:09 +0200

    configure singleScreen app stack

---
+++ src/screens/auth/login.screen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class LoginScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Login screen here...</Text>
+      </View>
+    );
+  }
+}
+
+export default LoginScreen;
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,9 +1,11 @@
 import { Navigation } from 'react-native-navigation';
 
 import App from '../App';
+import LoginScreen from './auth/login.screen';
 
 const registerScreens = (store = {}, Provider = {}) => {
   Navigation.registerComponent('wevedo.launchScreen', () => App, store, Provider);
+  Navigation.registerComponent('wevedo.loginScreen', () => LoginScreen, store, Provider);
 };
 
 export default registerScreens;
