51865d9ca7b572271210542ab420a7d0f0826bf4
Yarik Genza <gsmeter@gmail.com>
2018-03-18 01:15:36 +0200

    add blank signup screen

--- src/screens/auth/login.screen.js
+++ src/screens/auth/login.screen.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'native-base';
 
 class LoginScreen extends Component {
+  onRegisterPress = () => {
+    this.props.navigator.push({
+      screen: 'wevedo.registerScreen',
+      navigatorStyle: {
+        navBarHidden: true,
+      },
+    });
+  }
+
   render() {
     return (
       <View>
         <Text>Login screen here...</Text>
+        <Button onPress={this.onRegisterPress}>
+          <Text>Register</Text>
+        </Button>
       </View>
     );
   }
