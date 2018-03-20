f32df6232e49d3e72df7185f486ec50c4693ffea
Yarik Genza <gsmeter@gmail.com>
2018-03-20 17:14:48 +0200

    fix signup UI

--- src/components/auth/register.form.js
+++ src/components/auth/register.form.js
@@ -1,12 +1,12 @@
+/* eslint-disable global-require */
 import React, { Component } from 'react';
 import { Alert, ImageBackground, StyleSheet, View, Platform } from 'react-native';
 import { CheckBox, Body, ListItem, Picker, Left, Icon } from 'native-base';
-import { connect } from 'react-redux';
 import I18n from 'react-native-i18n';
 import { Button, Container, Content, FieldInput, Text } from '../../components/common';
- //import Eula from 'Eula'
 import { contrastColor, primaryFont } from '../../theme';
 // import SignupImageForm from '../Components/Auth/SignupImageForm';
+// import Eula from 'Eula'
 
 // const Item = Picker.Item;
 
@@ -24,7 +24,6 @@ import { contrastColor, primaryFont } from '../../theme';
 
 class SignupForm extends Component {
   state = {
-    loading: false,
     isModalVisible: false,
     step: 1,
     values: {
@@ -81,12 +80,12 @@ class SignupForm extends Component {
 
   /* handleAccept() {
         this.setState({isModalVisible: !this.state.isModalVisible});
-        const {email, password, fullName} = this.state.values;       
+        const {email, password, fullName} = this.state.values;
         const { isAdvertiser, category, image } = this.state;
-        this.setState({loading: true});        
+        this.setState({loading: true});
         const arrFN = fullName.split(' ').map((a) => { return a.charAt(0).toUpperCase() + a.substr(1) });
-        const capitalFullName = arrFN.join(' ');        
-        
+        const capitalFullName = arrFN.join(' ');
+
 
         if (isAdvertiser) {
             this.props.signupProvider(email, password, capitalFullName, image, category, result => {
@@ -144,17 +143,14 @@ class SignupForm extends Component {
   renderSignUp = () => {
     const disabled = false;
 
-  // const disabled = this.state.loading || pristine || submitting || !email || !password || !confirmPassword || (this.state.step === 2 && ! this.state.image)
+    // const disabled = this.state.loading || pristine || submitting || !email || !password || !confirmPassword || (this.state.step === 2 && ! this.state.image)
 
     return (
       <Content
         id="SignUp.content"
         padder
         keyboardShouldPersistTaps="always"
-        contentContainerStyle={{
-          flex: 1,
-          justifyContent: 'space-between',
-        }}
+        contentContainerStyle={{ flex: 1, justifyContent: 'space-between' }}
       >
         <View id="Signup.backButtonAndTitleWrapper" style={styles.header}>
           <Button id="Signup.backButton" style={{ flex: 1 }} transparent onPress={this.props.onBackPress}>
@@ -164,50 +160,59 @@ class SignupForm extends Component {
             {I18n.t('logIn.sign_up')}
           </Text>
         </View>
-                <View id="Signup.formWrapper" style={{
-                    flex: 0,
-                    justifyContent: 'flex-start',
-                }}>
-                    <View style={{backgroundColor: 'rgba(0,0,0,0.2)', padding: 10, paddingTop: 0, paddingBottom: 0, flexDirection: 'column'}}>
-                        {this.state.step === 1 && <View>
-                        <FieldInput color={contrastColor}
-                               name="fullName"
-                               placeholder={I18n.t('common.fullName')}
-                               errorColor={contrastColor}
-                               id='SignUp.fullNameInput'
-                               autoCapitalize='words'                              
-                        />
-                        <FieldInput color={contrastColor}
-                               name="email"
-                               placeholder={I18n.t('common.email')}
-                               errorColor={contrastColor}
-                               id='SignUp.emailInput'
-                        />
-                        <FieldInput color={contrastColor} name="password"
-                               placeholder={I18n.t('common.password')}
-                               errorColor={contrastColor}
-                               secureTextEntry={true}
-                               id='SignUp.passwordInput'
-                        />
-                        <FieldInput color={contrastColor}
-                               name="confirmPassword"
-                               placeholder={I18n.t('logIn.confirm_password')}
-                               errorColor={contrastColor}
-                               secureTextEntry={true}
-                               id='SignUp.confirmPasswordInput'
-                        />
 
-                       
-                        <View style={{ flexDirection: 'row', }}>
-                        <CheckBox checked={this.state.isAdvertiser} onPress={this.onCheckboxPress} color="#f3c200" />
-                            <Left>
-                                <Text style={styles.checkBoxText}>{I18n.t('logIn.advertiser')}</Text>
-                            </Left>
-                        </View>
+        <View id="Signup.formWrapper" style={{ flex: 0, justifyContent: 'flex-start' }}>
+          <View style={styles.formWrapper}>
 
-                        </View>}
+            {this.state.step === 1 && (
+            <View>
+              <FieldInput
+                name="fullName"
+                color={contrastColor}
+                placeholder={I18n.t('common.fullName')}
+                errorColor={contrastColor}
+                id="SignUp.fullNameInput"
+                autoCapitalize="words"
+              />
+              <FieldInput
+                color={contrastColor}
+                name="email"
+                placeholder={I18n.t('common.email')}
+                errorColor={contrastColor}
+                id="SignUp.emailInput"
+              />
+              <FieldInput
+                color={contrastColor}
+                name="password"
+                placeholder={I18n.t('common.password')}
+                errorColor={contrastColor}
+                secureTextEntry
+                id="SignUp.passwordInput"
+              />
+              <FieldInput
+                color={contrastColor}
+                name="confirmPassword"
+                placeholder={I18n.t('logIn.confirm_password')}
+                errorColor={contrastColor}
+                secureTextEntry
+                id="SignUp.confirmPasswordInput"
+              />
 
-                        {/*this.state.isAdvertiser && this.state.step === 1 && (
+              <View style={{ flexDirection: 'row' }}>
+                <CheckBox
+                  checked={this.state.isAdvertiser}
+                  onPress={this.onCheckboxPress}
+                  color="#f3c200"
+                />
+                <Left>
+                  <Text style={styles.checkBoxText}>{I18n.t('logIn.advertiser')}</Text>
+                </Left>
+              </View>
+
+            </View>
+          )}
+
+            {/* this.state.isAdvertiser && this.state.step === 1 && (
                           <View style={{ flexDirection: 'row'}}>
                               {Platform.OS === 'android' && <Text style={styles.categoryText}>{I18n.t('common.category')}</Text>}
                             <Picker
@@ -225,54 +230,58 @@ class SignupForm extends Component {
                                 ))}
                             </Picker>
                           </View>
-                        )*/}
+                        ) */}
 
-                        {/*this.state.isAdvertiser && this.state.step === 1 && (
+            {/* this.state.isAdvertiser && this.state.step === 1 && (
                             <View style={{ alignItems: 'center', borderTopColor: 'white', borderTopWidth: 1 }}>
                                 <Text note style={{ color: 'white', margin: 10, marginBottom: 0, }}>{I18n.t('logIn.account_activation')}</Text>
                             </View>
-                        )*/}
+                        ) */}
 
-                        {/*this.state.isAdvertiser && this.state.step === 2 && (
+            {/* this.state.isAdvertiser && this.state.step === 2 && (
                             <SignupImageForm
                                 onImageSelect={this.onImageSelect}
                             />
-                        )*/}
+                        ) */}
 
-                        <Button id="Signup.submitButton"
-                            style={styles.registerButton} block
-                            disabled={disabled}
-                            onPress={() => {}/*this.state.isAdvertiser && this.state.step === 1 ? this.onContinuePress : handleSubmit(this.onSubmitForm) */}
-                            spinner={this.state.loading}
-                        >
-                            <Text style={styles.registerButtonText}>
-                                {this.state.isAdvertiser && this.state.step === 1 ? I18n.t('common.continue') : I18n.t('logIn.sign_up')}
-                            </Text>
-                        </Button>
+            <Button
+              id="Signup.submitButton"
+              style={styles.registerButton}
+              block
+              onPress={this.handleSubmit/* this.state.isAdvertiser && this.state.step === 1 ? this.onContinuePress : handleSubmit(this.onSubmitForm) */}
+              spinner={this.props.isLoading}
+            >
+              <Text style={styles.registerButtonText}>
+                {this.state.isAdvertiser && this.state.step === 1 ? I18n.t('common.continue') : I18n.t('logIn.sign_up')}
+              </Text>
+            </Button>
 
 
-
-                        {/* <Eula isModalVisible={this.state.isModalVisible}
+            {/* <Eula isModalVisible={this.state.isModalVisible}
                               handleDecline={this.handleDecline.bind(this)}
                               handleAccept={this.handleAccept.bind(this)}
                               id="Signup.EULA"
                       /> */}
-                    </View>
-                </View>
-            </Content>
-        )
-    }
+          </View>
+        </View>
+      </Content>
+    );
+  }
 
-    render() {
-        return (
-            <Container id="SignUp.container" style={{ backgroundColor: 'red' }}>
-                <ImageBackground id="SignUp.bg-image" resizeMode="cover" style={styles.background}
-                                 source={require('../../images/loginBackground.png')}>
-                    {this.renderSignUp()}
-                </ImageBackground>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container id="SignUp.container" style={{ backgroundColor: 'red' }}>
+        <ImageBackground
+          id="SignUp.bg-image"
+          resizeMode="cover"
+          style={styles.background}
+          source={require('../../images/loginBackground.png')}
+        >
+          {this.renderSignUp()}
+        </ImageBackground>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -291,7 +300,6 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     top: 20,
     flexDirection: 'row',
-    textAlign: 'center',
   },
   registerButton: {
     marginTop: 10,
@@ -308,6 +316,13 @@ const styles = StyleSheet.create({
     color: 'white',
     marginTop: 15,
     marginRight: 100,
+  },
+  formWrapper: {
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    padding: 10,
+    paddingTop: 0,
+    paddingBottom: 0,
+    flexDirection: 'column',
   },
 });
 
