b55f2664f6ef95343e4630fa0dcef93d1dc72796
Yarik Genza <gsmeter@gmail.com>
2018-03-21 22:19:43 +0200

    change launching scheme

--- src/AppBootstrap.js
+++ src/AppBootstrap.js
@@ -5,13 +5,8 @@
  * */
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
 
 class AppBootstrap extends Component {
-  componentDidMount() {
-    SplashScreen.hide();
-  }
-
   render() {
     return (
       <View />
