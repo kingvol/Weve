f6d4ed62cb6711e1d83d19c1c782372ae698d5dc
Yarik Genza <gsmeter@gmail.com>
2018-03-31 19:15:15 +0300

    add calendar

--- src/screens/main/home/ProviderProfile.screen.js
+++ src/screens/main/home/ProviderProfile.screen.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import { Content } from 'native-base';
 import { View, Image } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import { lightTextColor, primaryFont } from '../../../theme';
+import { secondaryColor } from '../../../theme/colors';
+
+const calendarTheme = {
+  selectedDayBackgroundColor: lightTextColor,
+  todayTextColor: secondaryColor,
+  arrowColor: secondaryColor,
+  textDayFontFamily: primaryFont.fontFamily,
+  textMonthFontFamily: primaryFont.fontFamily,
+  textDayHeaderFontFamily: primaryFont.fontFamily,
+};
 
 class ProviderProfileScreen extends Component {
   render() {
     return (
       <Content contentContainerStyle={{ flexGrow: 1 }}>
-        <View style={{ minHeight: 280 }}>
+        <View style={{ minHeight: 500, flex: 2 }}>
           <Image
             style={styles.image}
             source={{ uri: this.props.provider.profileImageURL }}
+          />
+          <Calendar
+            theme={calendarTheme}
+            style={styles.calendar}
+            // markedDates={//markedDates}
+            // onDayPress={this.handleDayPress.bind(this)}
           />
         </View>
       </Content>
@@ -19,8 +37,8 @@ class ProviderProfileScreen extends Component {
 
 const styles = {
   image: {
-    flex: 1,
     resizeMode: 'contain',
+    flex: 1,
   },
 };
 
