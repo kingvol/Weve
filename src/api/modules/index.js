18bd6d0706d11fb75c4620f6c1441d34a254d16a
Yarik Genza <gsmeter@gmail.com>
2018-03-31 13:33:31 +0300

    implement provider api-connector

--- src/api/modules/index.js
+++ src/api/modules/index.js
@@ -1,9 +1,11 @@
 import AuthApi from './auth.api';
 import CategoryApi from './category.api';
 import UserApi from './user.api';
+import ProviderApi from './provider.api';
 
 export default {
   AuthApi,
   CategoryApi,
   UserApi,
+  ProviderApi,
 };
