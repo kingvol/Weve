45f1a0b65e60e61278d55efa8678f454036a034e
Yarik Genza <gsmeter@gmail.com>
2018-04-01 22:16:37 +0300

    fetch room list

--- src/screens/main/inbox.tab.js
+++ src/screens/main/inbox.tab.js
@@ -1,37 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { Container, Icon } from 'native-base';
+import { View, Text } from 'react-native';
+import { Container } from 'native-base';
 import { connect } from 'react-redux';
 import { fetchRooms } from '../../actions/chat.actions';
 
 class InboxTab extends Component {
-  state: {
-    rooms: [],
-  };
   componentDidMount() {
     this.props.fetchRooms();
   }
 
-  onPress() {
-    this.props.fetchRooms();
-  }
-
   render() {
-    return (
+    return !this.props.chat.rooms.length ? (
       <Container>
         <View>
-          <Text>Touch me</Text>
-          <TouchableOpacity onPress={() => this.onPress()}>
-            <Icon type="FontAwesome" name="inbox" />
-          </TouchableOpacity>
+          <Text>no active dialogs</Text>
         </View>
       </Container>
+    ) : (
+      <View><Text>Rooms list here...{JSON.stringify(this.props.chat.rooms)}</Text></View>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  rooms: state.chat,
+  chat: state.chat,
 });
 
 export default connect(mapStateToProps, { fetchRooms })(InboxTab);
