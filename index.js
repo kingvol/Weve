4f921546f99a473e3993336f7545bcb6f8197e5b
Preeternal <preeternal@ya.ru>
2018-04-07 21:10:46 +0300

    seems it works on Galaxy S8

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ export const startTabBasedApp = () => {
   });
 };
 
-const init = async () => {
+export const init = async () => {
   try {
     const token = await AsyncStorage.getItem('wevedo_access_token');
     if (token) {
