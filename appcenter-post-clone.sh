d39be5bbb2b21c05cbb66b89bbb7e5017a299558
Preeternal <preeternal@ya.ru>
2018-04-08 13:40:14 +0300

    config-vars added

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -8,7 +8,7 @@ then
   rm -f $APPCENTER_SOURCE_DIRECTORY/config/index.js
   rm -f $APPCENTER_SOURCE_DIRECTORY/src/env/vars
   cp -v $APPCENTER_SOURCE_DIRECTORY/config-vars/prod/index.js $APPCENTER_SOURCE_DIRECTORY/config
-  cp -v $APPCENTER_SOURCE_DIRECTORY/env/prod/vars.js $APPCENTER_SOURCE_DIRECTORY/src/env
+  cp -v $APPCENTER_SOURCE_DIRECTORY/config-vars/prod/vars.js $APPCENTER_SOURCE_DIRECTORY/src/env
   echo "Done"
 else
   echo “Using development config”
---
+++ config-vars/prod/vars.js
@@ -0,0 +1,5 @@
+const vars = {
+  DB_ENV: 'production',
+};
+
+export default vars;
