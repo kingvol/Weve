10d67e9dca3a3e276f1706ff83302133305213cd
Preeternal <preeternal@ya.ru>
2018-04-08 13:33:09 +0300

    add env vars to appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -6,7 +6,9 @@ if [[ $APPCENTER_BRANCH = 'master' ]];
 then
   echo “Switching to PRODUCTION environment”  
   rm -f $APPCENTER_SOURCE_DIRECTORY/config/index.js
+  rm -f $APPCENTER_SOURCE_DIRECTORY/src/env/vars
   cp -v $APPCENTER_SOURCE_DIRECTORY/config-vars/prod/index.js $APPCENTER_SOURCE_DIRECTORY/config
+  cp -v $APPCENTER_SOURCE_DIRECTORY/env/prod/vars.js $APPCENTER_SOURCE_DIRECTORY/src/env
   echo "Done"
 else
   echo “Using development config”
