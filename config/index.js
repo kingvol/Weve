7763fbc267dd9d784213ab68f446b034dd8591de
Yarik Genza <gsmeter@gmail.com>
2018-05-05 12:09:44 +0300

    add admin email

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export default {
   backendUrl: 'https://wevedo.herokuapp.com',
-  adminEmail: 'w.gsmeter@gmail.com',
+  adminEmail: 'charykurbanow@hotmail.com',
   cloudinary: {
     apiKey: '294357151451223',
     apiSecret: '8Q03lPaNnsQ3vNbwlypnGKdDwO0',
