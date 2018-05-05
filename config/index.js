4bcb4ae56e18e91780870ecc9f920e143e7e540a
Yarik Genza <gsmeter@gmail.com>
2018-05-05 12:03:38 +0300

    divive to dev and prod envs

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export default {
-  backendUrl: 'https://wevedo-backend.appspot.com',
-  adminEmail: 'ckurban@me.com',
+  backendUrl: 'https://wevedo.herokuapp.com',
+  adminEmail: 'w.gsmeter@gmail.com',
   cloudinary: {
     apiKey: '294357151451223',
     apiSecret: '8Q03lPaNnsQ3vNbwlypnGKdDwO0',
