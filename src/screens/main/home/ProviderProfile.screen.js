51451a8d6957358b2e50a63969c7a3087789907e
Preeternal <preeternal@ya.ru>
2018-06-21 21:41:46 +0300

    delete extra code

--- src/screens/main/home/ProviderProfile.screen.js
+++ src/screens/main/home/ProviderProfile.screen.js
@@ -30,9 +30,6 @@ class ProviderProfileScreen extends Component {
   constructor(props) {
     super(props);
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
-    this.state = {
-      swiperImageResizeMode: FastImage.resizeMode.contain,
-    };
   }
 
   componentDidMount() {
