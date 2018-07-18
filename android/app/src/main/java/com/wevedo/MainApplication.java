08064143c8ebeefbb594b7b0396c50553c32a8aa
Yarik Genza <gsmeter@gmail.com>
2018-07-18 23:06:48 +0300

    install video player

--- android/app/src/main/java/com/wevedo/MainApplication.java
+++ android/app/src/main/java/com/wevedo/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.support.annotation.NonNull;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.vydia.RNUploader.UploaderReactPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
 import com.oblador.keychain.KeychainPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends NavigationApplication  {
   public List<ReactPackage> createAdditionalReactPackages() {
     return Arrays.<ReactPackage>asList(
     new MainReactPackage(),
+            new ReactVideoPackage(),
             new UploaderReactPackage(),
             new FingerprintAuthPackage(),
             new KeychainPackage(),
