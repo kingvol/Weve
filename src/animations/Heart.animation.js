ee0d5b87f0318b2e9f7d7e8e7974a4cacac39d8a
Yarik Genza <gsmeter@gmail.com>
2018-08-14 00:39:41 +0300

    implement heart animation

---
+++ src/animations/Heart.animation.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+class HeartAnimation extends Component {
+  componentDidMount() {
+    const { filled } = this.props;
+    if (filled) {
+      this.animation.play(25, 50);
+    }
+  }
+
+  onAnimationPress = () => {
+    if (this.props.filled) {
+      this.fillOut();
+    } else {
+      this.fillIn();
+    }
+    this.props.onAnimationPress();
+  };
+
+  fillIn = () => {
+    this.animation.play(0, 50);
+  };
+
+  fillOut = () => {
+    this.animation.play(30, 0);
+  };
+
+  render() {
+    return (
+      <TouchableWithoutFeedback onPress={this.onAnimationPress}>
+        <LottieView
+          resizeMode="contain"
+          style={{
+            width: 65,
+            heigth: 65,
+            marginRight: 30,
+            resizeMode: 'cover',
+            top: 0,
+            right: -35,
+            position: 'absolute',
+          }}
+          ref={(animation) => {
+            this.animation = animation;
+          }}
+          source={require('./sources/heart2.json')}
+          loop={false}
+        />
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+export default HeartAnimation;
