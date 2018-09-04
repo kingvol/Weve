fdbd7d0bf63fb0f66ba5a21059e46bd309c67df7
Preeternal <preeternal@ya.ru>
2018-09-04 19:23:18 +0300

    My profile and settings page with poor internet connection

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,6 +4,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from '../reducers';
 
 let middleware = [promise(), thunk];
@@ -16,11 +19,21 @@ if (__DEV__) {
   middleware = [...middleware];
 }
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+  blacklist: ['chat'],
+  whitelist: ['ui', 'user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
 
 const store = createStoreWithMiddleware(
-  rootReducer,
+  persistedReducer,
+  // rootReducer,
   __DEV__ && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
